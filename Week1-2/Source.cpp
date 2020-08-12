4209ed87865383a58981a0b50c74e2c5858d60d4
Thiraphat Ketsingnoi <63010467@kmitl.ac.th>
2020-08-12 22:33:07 +0700

    input,output

---
+++ Week1-2/Source.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("\nK\t\t\t\t\tK\t\t\tL\n");
+	printf("\nK\t\t\t\tK\t\t\t\tL\n");
+	printf("\n*\t\t\tK\t\t\t\t\tL\n");
+	printf("\nK\t\tK\t\t\t\t\t\tL\n");
+	printf("\nK\tK\t\t\t\t\t\t\tL\n");
+	printf("\nK\t\tK\t\t\t\t\t\tL\n");
+	printf("\nK\t\t\tK\t\t\t\t\tL\n");
+	printf("\nK\t\t\t\tK\t\t\t\tL\n");
+	printf("\nK\t\t\t\t\tK\t\t\tL\t\L\tL\tL\tL\n");
+	printf("\n\tKing\tMongkut's\tInstitude\tof\tTechnology\tLadkrabang\n");
+	return 0;
+}
